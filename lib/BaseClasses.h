af88f1a23ea91a04a3ed571bae58b19fc063468a
neonLaserUnicorn <Astrargot@gmail.com>
2022-06-17 02:15:04 +0300

    create basse classes

--- lib/BaseClasses.h
+++ lib/BaseClasses.h
@@ -1,13 +1,29 @@
+#pragma once
 #include <SFML/Graphics.hpp>
-
 
 class Object
 {
 protected:
-    sf::Drawable* _form;
+    sf::Shape* _form;
     sf::Vector2f _pos;
 public:
-    Object(sf::Drawable* form):
-        _form(form){}
-    sf::Vector2f
-}
+    Object(){}
+    sf::Vector2f getPosition(){return _pos;}
+    sf::Drawable& getSprite(){return *_form;}
+    virtual void collide(Object* another) = 0;
+};
+
+class Movable : public Object
+{
+protected:
+    float _maxSpeed = 1.f;
+    float _speed;
+    float _maxFallSpeed = 2.f;
+    float _fallSpeed;
+public:
+    Movable(){}
+    virtual void update() = 0;
+    virtual void move() = 0;
+    virtual void fall() = 0;
+    void setPosition(sf::Vector2f pos){_pos = pos;}
+};
